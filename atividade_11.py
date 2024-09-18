47e3ac031b0e9c291511159b85358b63d1104215
saviocunhaa <saviocunha.01@hotmail.com>
2024-09-18 14:52:32 -0300

    exemplo

--- atividade_11.py
+++ atividade_11.py
@@ -1,6 +1,14 @@
 # Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+idade = int(input("digite sua idade ")) 
 # Criança (0-12 anos)
-# Adolescente (13-17 anos)
+if idade <= 12:
+    print("Criança")
+elif idade <= 17:
+    print("Adolescente")
 # Adulto (18-59 anos)
+elif idade <= 59:
+    print("Adulto")
 # Idoso (60 anos ou mais)
+else:
+    print("Idoso")
 
---
+++ exemplosCondicionaiseLoops.py
@@ -0,0 +1,52 @@
+
+# # v1 = 2
+# # v2 = 3
+# # v3 = 4
+# # v4 = 5
+
+# # print(v1)
+# # print(v2)
+# # print(v3)
+# # print(v4)
+
+# # for numero in range(5):
+# #     print(numero)
+
+# listaNumero = [1,5,8,4,6]
+
+# # print(listaNumero[0])
+# # print(listaNumero[1])
+# # print(listaNumero[2])
+# # print(listaNumero[3])
+# # print(listaNumero[4])
+
+# for i in listaNumero:
+#     print(i)
+
+
+
+# valor = 0
+
+# while valor < 5:
+#     print(f"o valor e {valor}")
+#     valor += 1 
+#     # valor = valor + 1
+
+
+# print("Savio")
+
+# computador = ["computador", "teclado", "mouse"]
+
+# for indice, valor in enumerate(computador):
+#     print(f"indice {indice}, valor {valor}")
+
+
+
+# #quero digitar 5 numeros e adicionar a minha lista
+# listaNumeros = []
+
+# for numero in range(5):
+#     n = input("digite o valor")
+#     listaNumeros.append(n)
+
+# print(listaNumeros)
