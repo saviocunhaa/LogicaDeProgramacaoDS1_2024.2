5d06e23f7289f1309c0c00652fb7f3fbb440990e
saviocunhaa <saviocunha.01@hotmail.com>
2024-09-18 09:22:43 -0300

    algumas correcoes

--- atividade_10.py
+++ atividade_10.py
@@ -1,1 +1,8 @@
 # Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+numero = float(input("digite o valor numerico "))
+
+if (numero % 2) == 0:
+    print("numero par")
+else:
+    print("numero impar")
--- atividade_17.py
+++ atividade_17.py
@@ -1,1 +1,8 @@
 # Crie um programa que receba um ano e verifique se ele é bissexto. Um ano é bissexto se for divisível por 4, exceto se for divisível por 100 e não por 400.
+
+ano = int(input("digite um ano"))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"{ano} é bissexto")
+else:
+    print("nao é bissexto")
