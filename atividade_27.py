e69bbd26dbd62eb4e29859033e9bea1c8e65be08
saviocunhaa <saviocunha.01@hotmail.com>
2024-09-25 14:01:05 -0300

    adicionar q27

---
+++ atividade_27.py
@@ -0,0 +1,11 @@
+# Solicite ao usuário que insira o nome de até 7 convidados.
+# Armazene os nomes em uma lista.
+# Permita ao usuário remover um convidado da lista, caso necessário.
+# Exiba a lista final de convidados.
+
+# Digite o nome do convidado 1: João
+# Digite o nome do convidado 2: Maria
+# ...
+# Digite o nome do convidado 7: Pedro
+# Deseja remover algum convidado da lista? (sim/não): sim
+# Digite o nome do convidado a ser removido: Maria
