c296b0c971f6396fb17d0a8d525cc705a6276413
saviocunhaa <saviocunha.01@hotmail.com>
2024-10-16 13:16:42 -0300

    atividade funcoes

---
+++ funcoes-atv02.py
@@ -0,0 +1,1 @@
+# Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
---
+++ funcoes-atv03.py
@@ -0,0 +1,3 @@
+# Crie duas funções:
+# - calcular_area_base, que recebe o raio de um círculo e retorna sua área.
+# - calcular_volume_cilindro, que utiliza a função calcular_area_base para calcular o volume de um cilindro dado o raio e a altura.
