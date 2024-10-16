99f0b46c605b3aaca8616102206799b4706cbc25
saviocunhaa <saviocunha.01@hotmail.com>
2024-10-16 14:29:22 -0300

    resposta

--- funcoes-atv02.py
+++ funcoes-atv02.py
@@ -1,1 +1,11 @@
 # Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
+
+def verificar_paridade(numero):
+    if numero % 2 == 0:
+        return f"o {numero} é par"
+    else:
+        return f"o {numero} é impar"
+
+numemr_informado = int(input("digite um numero"))
+resultado = verificar_paridade(numemr_informado)
+print(resultado)
